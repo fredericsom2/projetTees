4d474ed5b8d0dd54db024fdf24e5a399e13e186c
fredericsom2 <frederic.som@lapiscine.pro>
2025-03-25 14:10:04 +0100

    projet tees

---
+++ controller/crate-order.controller.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+</head>
+<body>
+    <h1>Formulaire</h1>
+    
+    <form method="post" action="">
+        <label for="quantity">Quantité :</label>
+        <input type="submit" name="submit" value="Envoyer">
+    </form>
+
+   
+    
+</body>
+</html>
