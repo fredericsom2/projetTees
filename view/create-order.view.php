13190ff6df3950ab01d3457bd489363d5fe49871
fredericsom2 <frederic.som@lapiscine.pro>
2025-03-25 14:23:18 +0100

    projetTees

---
+++ view/create-order.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+</head>
+<body>
+    <!-- je crée un titre au formulaire  -->
+    <h1>Formulaire</h1>
+    <!--  je crée un formulaire  -->
+    
+    <form method="post" action="">
+        <label for="quantity">Quantité :</label>
+        <input type="submit" name="submit" value="Envoyer">
+    </form>
+
+   
+    
+</body>
+</html>
