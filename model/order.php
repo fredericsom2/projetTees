b958403d2ce84734dedc4614d6c7c5e720948988
fredericsom2 <frederic.som@lapiscine.pro>
2025-04-14 12:31:23 +0200

    projetTees

--- model/order.php
+++ model/order.php
@@ -2,6 +2,8 @@
 
 class Order {
 
+
+      // je lui donne une variable "product" qui représentera le produit de la commande
 	public $product;
 
 	public $quantity;
@@ -9,3 +11,16 @@ class Order {
 	public $createdAt;
 
 	public $status;
+}
+// cela permet de remplacer le tableau qui se trouve sur order.repository.php
+
+
+// je créé un objet, qui aura toutes les propriétés définies dans la classe.
+// pour l'instant ces propriétés sont vides, mais je peux les définir
+$order = new Order();
+
+// remplir les valeurs de chacune des propriétés
+$order->product = "Teeshirt Mario";
+$order->quantity = 2;
+$order->createdAt = new DateTime();
+$order->status = "Cart";
